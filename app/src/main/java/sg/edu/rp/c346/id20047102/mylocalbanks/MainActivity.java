27ce84c8a7571d1bd271f9f7533b875e8620b033
CheeYuan <20047102@myrp.edu.sg>
2022-05-26 14:44:36 +0800

    Added Favourite system & replaced hardcoded text with Resources from strings.xml into MainActivity.java as enhancement.

--- app/src/main/java/sg/edu/rp/c346/id20047102/mylocalbanks/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20047102/mylocalbanks/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20047102.mylocalbanks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -16,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     TextView tvDBS;
     TextView tvOCBC;
     TextView tvUOB;
+    boolean favouriteDBS = true;
+    boolean favouriteOCBC = true;
+    boolean favouriteUOB = true;
+
     String bankOption = "";
 
     @Override
@@ -44,19 +49,15 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         if (id == R.id.EnglishSelection) {
-            tvDBS.setText("DBS");
-            tvOCBC.setText("OCBC");
-            tvUOB.setText("UOB");
+            tvDBS.setText(getString(R.string.dbsbank));
+            tvOCBC.setText(getString(R.string.ocbcbank));
+            tvUOB.setText(getString(R.string.uobbank));
             return true;
         } else if (id == R.id.ChineseSelection) {
-            tvDBS.setText("星展银行");
-            tvOCBC.setText("华侨银行");
-            tvUOB.setText("大华银行");
+            tvDBS.setText(getString(R.string.dbsbankChinese));
+            tvOCBC.setText(getString(R.string.ocbcbankChinese));
+            tvUOB.setText(getString(R.string.uobbankChinese));
             return true;
-        } else {
-            tvDBS.setText("Error Translation");
-            tvOCBC.setText("Error Translation");
-            tvUOB.setText("Error Translation");
         }
 
         return super.onOptionsItemSelected(item);
@@ -68,49 +69,77 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateContextMenu(menu, v, menuInfo);
 
         if (v == tvDBS) {
-            bankOption = "DBS";
+            bankOption = getString(R.string.dbsbank);
         } else if (v == tvOCBC) {
-            bankOption = "OCBC";
+            bankOption = getString(R.string.ocbcbank);
         } else if (v == tvUOB) {
-            bankOption = "UOB";
+            bankOption = getString(R.string.uobbank);
         }
 
         menu.add(0, 0, 0, "Website");
         menu.add(0, 1, 1, "Contact the bank");
+        menu.add(0, 2, 2, "Toggle Favourite");
     }
 
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-        if (bankOption.equalsIgnoreCase("DBS")) {
+        if (bankOption.equalsIgnoreCase(getString(R.string.dbsbank))) {
             if (item.getItemId() == 0) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.dbs.com.sg"));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.dbsURL)));
                 startActivity(intent);
                 return true;
             } else if (item.getItemId() == 1) {
-                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 18001111111L));
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + getString(R.string.dbsNum)));
                 startActivity(intentCall);
+                return true;
+            } else if (item.getItemId() == 2) {
+                if (favouriteDBS) {
+                    tvDBS.setTextColor(Color.RED);
+                    favouriteDBS = false;
+                } else {
+                    tvDBS.setTextColor(Color.BLACK);
+                    favouriteDBS = true;
+                }
                 return true;
             }
 
-        } else if (bankOption.equalsIgnoreCase("OCBC")) {
+        } else if (bankOption.equalsIgnoreCase(getString(R.string.ocbcbank))) {
             if (item.getItemId() == 0) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ocbc.com"));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.ocbcURL)));
                 startActivity(intent);
                 return true;
             } else if (item.getItemId() == 1) {
-                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 18003633333L));
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + getString(R.string.ocbcNum)));
                 startActivity(intentCall);
+                return true;
+            } else if (item.getItemId() == 2) {
+                if (favouriteOCBC) {
+                    tvOCBC.setTextColor(Color.RED);
+                    favouriteOCBC = false;
+                } else {
+                    tvOCBC.setTextColor(Color.BLACK);
+                    favouriteOCBC = true;
+                }
                 return true;
             }
 
-        } else if (bankOption.equalsIgnoreCase("UOB")) {
+        } else if (bankOption.equalsIgnoreCase(getString(R.string.uobbank))) {
             if (item.getItemId() == 0) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.uob.com"));
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.uobURL)));
                 startActivity(intent);
                 return true;
             } else if (item.getItemId() == 1) {
-                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + 18002222121L));
+                Intent intentCall = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + getString(R.string.uobNum)));
                 startActivity(intentCall);
+                return true;
+            } else if (item.getItemId() == 2) {
+                if (favouriteUOB) {
+                    tvUOB.setTextColor(Color.RED);
+                    favouriteUOB = false;
+                } else {
+                    tvUOB.setTextColor(Color.BLACK);
+                    favouriteUOB = true;
+                }
                 return true;
             }
         }
